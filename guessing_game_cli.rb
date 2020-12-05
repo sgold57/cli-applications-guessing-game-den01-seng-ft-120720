557f28263640b97462f3d68bcc73c0f93a5e256e
Sam Gold <sgold5757@gmail.com>
2020-12-05 23:33:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,27 +2,14 @@
 
 require 'pry'
 
-
-def correct_guess
-  puts "You guessed the correct number!"
-end
-
-def exit_guess
-  puts "Goodbye!"
-end
-
-def incorrect_guess(number)
-  puts "Sorry! The computer guessed #{number}."
-end
-
 def run_guessing_game
   cpu_guess = rand 1...6
   input = gets.chomp
   if input == cpu_guess.to_s
-    correct_guess
+    puts "You guessed the correct number!"
   elsif input == 'exit'
-    exit_guess
+    puts "Goodbye!"
   else
-    incorrect_guess(cpu_guess)
+    puts "Sorry! the computer guessed #{cpu_guess}."
   end
 end

c3ffeb12ae1bcf4c875a84887a9b5d67abe7ef38
Sam Gold <sgold5757@gmail.com>
2020-12-05 23:13:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,43 @@
 # Code your solution here!
+
+require 'pry'
+
+def store_number
+  rand 1...6
+end
+
+def user_prompt
+  puts "Choose number from 1-6:"
+end
+
+def user_choice
+  gets.chomp
+end
+
+def correct_guess
+  puts "You guessed the correct number!"
+end
+
+def exit_guess
+  puts "Goodbye!"
+end
+
+def incorrect_guess(number)
+  puts "Sorry! The computer guessed #{number}."
+end
+
+def run_guessing_game
+  cpu_guess = store_number
+  user_prompt
+  user_guess = user_choice
+  binding.pry
+  if user_guess == cpu_guess.to_s
+    binding.pry
+    correct_guess
+  elsif user_guess == 'exit'
+    binding.pry
+    exit_guess
+  else
+    incorrect_guess(cpu_guess)
+  end
+end

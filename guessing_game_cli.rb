d6711a72bf37455ac04e26530c9a7d30ecc7c010
Sam Gold <sgold5757@gmail.com>
2020-12-05 23:28:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,13 +2,6 @@
 
 require 'pry'
 
-def store_number
-  rand 1...6
-end
-
-def user_prompt
-  puts "Choose number from 1-6:"
-end
 
 def correct_guess
   puts "You guessed the correct number!"
@@ -22,14 +15,8 @@ def incorrect_guess(number)
   puts "Sorry! The computer guessed #{number}."
 end
 
-def user_guess
-  user_prompt
-  user_choice = gets.chomp
-end
-
-
 def run_guessing_game
-  cpu_guess = store_number
+  cpu_guess = rand 1...6
   input = gets.chomp
   if input == cpu_guess.to_s
     correct_guess
